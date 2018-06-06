dbe983955ab2389a970fc89537123e732cb109f9
YudhaYuki <YudhaYuki@hotmail.com>
2018-06-06 19:59:02 +0200

    Rendering the table

--- script/main.js
+++ script/main.js
@@ -3,8 +3,9 @@
 
 
 let aBankAccount = new XMLHttpRequest();
-aBankAccount.open('GET', 'apiserver/result.json');
+aBankAccount.open('GET', 'http://localhost:8080/api/balance');
 aBankAccount.onload = function() {
+    var i;
     var dataABankAccount = JSON.parse(aBankAccount.responseText);
     console.log(dataABankAccount.currency)
     document.querySelector('.account-name').textContent = `Account Holder: ${dataABankAccount.account.name}`;
@@ -12,10 +13,37 @@ aBankAccount.onload = function() {
     document.querySelector('.account-balance').textContent = `Balance: ${dataABankAccount.account.balance}`;
     document.querySelector('.account-currency').textContent = `Currency: ${dataABankAccount.currency}`;
 
-    document.querySelector('.debits-and-credits__type').textContent = dataABankAccount.debitsAndCredits[1].from;
-    document.querySelector('.debits-and-credits__description').textContent = dataABankAccount.debitsAndCredits[1].description;
-    document.querySelector('.debits-and-credits__amount').textContent = dataABankAccount.debitsAndCredits[1].amount;
-    document.querySelector('.debits-and-credits__date').textContent = dataABankAccount.debitsAndCredits[1].date;
+    /*
+    for (i = 0; i < dataABankAccount.debitsAndCredits.length; i++) {
+        // document.querySelector('.debits-and-credits__type').textContent = dataABankAccount.debitsAndCredits[i].from;
+        document.querySelector('.debits-and-credits__description').textContent = dataABankAccount.debitsAndCredits[i].description;
+        document.querySelector('.debits-and-credits__amount').textContent = dataABankAccount.debitsAndCredits[i].amount;
+        document.querySelector('.debits-and-credits__date').textContent = dataABankAccount.debitsAndCredits[i].date;
+    }
+    */
+
+    document.write(`<table>`);
+    document.write(`
+        <tr>
+            <th>TO/FROM</th>
+            <th>DESCRIPTION</th>
+            <th>AMOUNT</th>
+            <th>DATE</th>
+        </tr>
+    `);
+
+    for (i = 0; i < dataABankAccount.debitsAndCredits.length; i++) {
+        document.write(`
+        <tr>
+            <td class="debits-and-credits__description">${dataABankAccount.debitsAndCredits[i].from}</td>        
+            <td class="debits-and-credits__description">${dataABankAccount.debitsAndCredits[i].description}</td>
+            <td class="debits-and-credits__amount">${dataABankAccount.debitsAndCredits[i].amount}</td>
+            <td class="debits-and-credits__date">${dataABankAccount.debitsAndCredits[i].date}</td>
+        </tr>     
+        `);
+    }
+    document.write(`</table>`);
+    
     
     
     
